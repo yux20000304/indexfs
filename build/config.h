054814fdb782bb27efe3801f030cde4b1fda4e29
yux20000304 <2056404413@qq.com>
2022-01-05 19:51:26 +0800

    add cache-test

---
+++ build/config.h
@@ -0,0 +1,296 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the <fuse.h> header file. */
+#define HAVE_FUSE_H 1
+
+/* Define to 1 if you have the `getpagesize' function. */
+#define HAVE_GETPAGESIZE 1
+
+/* Define to 1 if you have the <gflags/gflags.h> header file. */
+#define HAVE_GFLAGS_GFLAGS_H 1
+
+/* Define to 1 if you have the <glog/logging.h> header file. */
+#define HAVE_GLOG_LOGGING_H 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the `crypto' library (-lcrypto). */
+/* #undef HAVE_LIBCRYPTO */
+
+/* Define to 1 if you have the `dl' library (-ldl). */
+#define HAVE_LIBDL 1
+
+/* Define to 1 if you have the `event' library (-levent). */
+#define HAVE_LIBEVENT 1
+
+/* Define to 1 if you have the `fuse' library (-lfuse). */
+#define HAVE_LIBFUSE 1
+
+/* Define to 1 if you have the `gflags' library (-lgflags). */
+#define HAVE_LIBGFLAGS 1
+
+/* Define to 1 if you have the `glog' library (-lglog). */
+#define HAVE_LIBGLOG 1
+
+/* Define to 1 if you have the `m' library (-lm). */
+#define HAVE_LIBM 1
+
+/* Define to 1 if you have the `pthread' library (-lpthread). */
+#define HAVE_LIBPTHREAD 1
+
+/* Define to 1 if you have the `pvfs2' library (-lpvfs2). */
+/* #undef HAVE_LIBPVFS2 */
+
+/* Define to 1 if you have the `rados' library (-lrados). */
+/* #undef HAVE_LIBRADOS */
+
+/* Define to 1 if you have the `snappy' library (-lsnappy). */
+#define HAVE_LIBSNAPPY 1
+
+/* Define to 1 if you have the `ssl' library (-lssl). */
+/* #undef HAVE_LIBSSL */
+
+/* Define to 1 if you have the `stdc++' library (-lstdc++). */
+#define HAVE_LIBSTDC__ 1
+
+/* Define to 1 if you have the `thrift' library (-lthrift). */
+#define HAVE_LIBTHRIFT 1
+
+/* Define to 1 if you have the `thriftnb' library (-lthriftnb). */
+#define HAVE_LIBTHRIFTNB 1
+
+/* Define to 1 if you have the `z' library (-lz). */
+#define HAVE_LIBZ 1
+
+/* Define to 1 if you have the `zookeeper_mt' library (-lzookeeper_mt). */
+/* #undef HAVE_LIBZOOKEEPER_MT */
+
+/* Define to 1 if your system has a GNU libc compatible `malloc' function, and
+   to 0 otherwise. */
+#define HAVE_MALLOC 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have a working `mmap' system call. */
+#define HAVE_MMAP 1
+
+/* Define to 1 if you have the <netinet/in.h> header file. */
+#define HAVE_NETINET_IN_H 1
+
+/* Define to 1 if you have the <snappy.h> header file. */
+#define HAVE_SNAPPY_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/param.h> header file. */
+#define HAVE_SYS_PARAM_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <thrift/concurrency/ThreadManager.h> header
+   file. */
+#define HAVE_THRIFT_CONCURRENCY_THREADMANAGER_H 1
+
+/* Define to 1 if you have the <thrift/protocol/TProtocol.h> header file. */
+#define HAVE_THRIFT_PROTOCOL_TPROTOCOL_H 1
+
+/* Define to 1 if you have the <thrift/server/TServer.h> header file. */
+#define HAVE_THRIFT_SERVER_TSERVER_H 1
+
+/* Define to 1 if you have the <thrift/TProcessor.h> header file. */
+#define HAVE_THRIFT_TPROCESSOR_H 1
+
+/* Define to 1 if you have the <thrift/transport/TTransport.h> header file. */
+#define HAVE_THRIFT_TRANSPORT_TTRANSPORT_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if you have the <zookeeper/zookeeper.h> header file. */
+/* #undef HAVE_ZOOKEEPER_ZOOKEEPER_H */
+
+/* Define to 1 if `lstat' dereferences a symlink specified with a trailing
+   slash. */
+#define LSTAT_FOLLOWS_SLASHED_SYMLINK 1
+
+/* Define to the sub-directory where libtool stores uninstalled libraries. */
+#define LT_OBJDIR ".libs/"
+
+/* Name of package */
+#define PACKAGE "indexfs"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "indexfs"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "indexfs 0.4.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "indexfs"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.4.1"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Enable extensions on AIX 3, Interix.  */
+#ifndef _ALL_SOURCE
+# define _ALL_SOURCE 1
+#endif
+/* Enable GNU extensions on systems that have them.  */
+#ifndef _GNU_SOURCE
+# define _GNU_SOURCE 1
+#endif
+/* Enable threading extensions on Solaris.  */
+#ifndef _POSIX_PTHREAD_SEMANTICS
+# define _POSIX_PTHREAD_SEMANTICS 1
+#endif
+/* Enable extensions on HP NonStop.  */
+#ifndef _TANDEM_SOURCE
+# define _TANDEM_SOURCE 1
+#endif
+/* Enable general extensions on Solaris.  */
+#ifndef __EXTENSIONS__
+# define __EXTENSIONS__ 1
+#endif
+
+
+/* Version number of package */
+#define VERSION "0.4.1"
+
+/* Enable large inode numbers on Mac OS X 10.5.  */
+#ifndef _DARWIN_USE_64_BIT_INODE
+# define _DARWIN_USE_64_BIT_INODE 1
+#endif
+
+/* Number of bits in a file offset, on hosts where this is settable. */
+/* #undef _FILE_OFFSET_BITS */
+
+/* Define for large files, on AIX-style hosts. */
+/* #undef _LARGE_FILES */
+
+/* Define to 1 if on MINIX. */
+/* #undef _MINIX */
+
+/* Define to 2 if the system does not provide POSIX.1 features except with
+   this defined. */
+/* #undef _POSIX_1_SOURCE */
+
+/* Define to 1 if you need to in order for `stat' and other things to work. */
+/* #undef _POSIX_SOURCE */
+
+/* Define for Solaris 2.5.1 so the uint32_t typedef from <sys/synch.h>,
+   <pthread.h>, or <semaphore.h> is not used. If the typedef were allowed, the
+   #define below would cause a syntax error. */
+/* #undef _UINT32_T */
+
+/* Define for Solaris 2.5.1 so the uint64_t typedef from <sys/synch.h>,
+   <pthread.h>, or <semaphore.h> is not used. If the typedef were allowed, the
+   #define below would cause a syntax error. */
+/* #undef _UINT64_T */
+
+/* Define for Solaris 2.5.1 so the uint8_t typedef from <sys/synch.h>,
+   <pthread.h>, or <semaphore.h> is not used. If the typedef were allowed, the
+   #define below would cause a syntax error. */
+/* #undef _UINT8_T */
+
+/* use HDFS as the underlying file system */
+/* #undef __HDFS__ */
+
+/* use NFS as the underlying file system */
+#define __NFS__ /**/
+
+/* use PVFS as the underlying file system */
+/* #undef __PVFS__ */
+
+/* use RADOS as the underlying file system */
+/* #undef __RADOS__ */
+
+/* Define to `int' if <sys/types.h> doesn't define. */
+/* #undef gid_t */
+
+/* Define to `__inline__' or `__inline' if that's what the C compiler
+   calls it, or to nothing if 'inline' is not supported under any name.  */
+#ifndef __cplusplus
+/* #undef inline */
+#endif
+
+/* Define to the type of a signed integer type of width exactly 16 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef int16_t */
+
+/* Define to the type of a signed integer type of width exactly 32 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef int32_t */
+
+/* Define to the type of a signed integer type of width exactly 64 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef int64_t */
+
+/* Define to the type of a signed integer type of width exactly 8 bits if such
+   a type exists and the standard includes do not define it. */
+/* #undef int8_t */
+
+/* Define to rpl_malloc if the replacement function should be used. */
+/* #undef malloc */
+
+/* Define to `int' if <sys/types.h> does not define. */
+/* #undef mode_t */
+
+/* Define to `long int' if <sys/types.h> does not define. */
+/* #undef off_t */
+
+/* Define to `int' if <sys/types.h> does not define. */
+/* #undef pid_t */
+
+/* Define to `unsigned int' if <sys/types.h> does not define. */
+/* #undef size_t */
+
+/* Define to `int' if <sys/types.h> does not define. */
+/* #undef ssize_t */
+
+/* Define to `int' if <sys/types.h> doesn't define. */
+/* #undef uid_t */
+
+/* Define to the type of an unsigned integer type of width exactly 16 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef uint16_t */
+
+/* Define to the type of an unsigned integer type of width exactly 32 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef uint32_t */
+
+/* Define to the type of an unsigned integer type of width exactly 64 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef uint64_t */
+
+/* Define to the type of an unsigned integer type of width exactly 8 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef uint8_t */
---
+++ sbin/cache-test.sh
@@ -0,0 +1,131 @@
+#!/bin/bash
+#
+# Copyright (c) 2014-2016 Carnegie Mellon University.
+#
+# All rights reserved.
+#
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file. See the AUTHORS file for names of contributors.
+#
+# Please run this script at the indexfs's home directory:
+#   > sbin/tree-test.sh <num_of_clients> <run_type>
+#
+# This launches an MPI-based performance test on an indexfs setup.
+# Root privilege is neither required nor recommended to run this script.
+#
+# MPI is required to run this script:
+#   > mpirun -version             or more specifically
+#   > mpirun.mpich -version
+#
+
+me=$0
+INDEXFS_HOME=$(cd -P -- `dirname $me`/.. && pwd -P)
+INDEXFS_CONF_FILE="/tmp/idxfs_conf"
+INDEXFS_SERVER_LIST="/tmp/giga_conf"
+INDEXFS_RUN_TYPE=${2-"$INDEXFS_RUN_TYPE"}
+if test -z "$INDEXFS_RUN_TYPE"; then INDEXFS_RUN_TYPE="regular"; fi
+INDEXFS_RUN_PREFIX=${INDEXFS_RUN_PREFIX="`date +%s`"}
+
+# check the location of the build directory
+INDEXFS_BUILD=$INDEXFS_HOME
+
+if test -d $INDEXFS_HOME/build
+then
+  INDEXFS_BUILD=$INDEXFS_HOME/build
+fi
+
+# check our test binary
+if test ! -e $INDEXFS_BUILD/io_test/io_driver
+then
+  echo "Cannot find the test binary -- oops"
+  echo "It is supposed to be found at $INDEXFS_BUILD/io_test/io_driver"
+  exit 1
+fi
+
+# check server list
+if test ! -e $INDEXFS_SERVER_LIST
+then
+  echo "Cannot find indexfs server list -- oops"
+  echo "It is supposed to be found at $INDEXFS_SERVER_LIST"
+  exit 1
+fi
+
+# check indexfs configuration file
+if test ! -e $INDEXFS_CONF_FILE
+then
+  echo "Cannot find indexfs configuration file -- oops"
+  echo "It is supposed to be found at $INDEXFS_CONF_FILE"
+  exit 1
+fi
+
+INDEXFS_ROOT=${INDEXFS_ROOT:-"/tmp/indexfs"}
+INDEXFS_RUN=$INDEXFS_ROOT/run
+INDEXFS_OUTPUT=$INDEXFS_RUN/tree_test
+INDEXFS_BACKEND=`$INDEXFS_HOME/sbin/idxfs.sh backend`
+
+# prepare test directories
+rm -rf $INDEXFS_OUTPUT/*
+mkdir -p $INDEXFS_OUTPUT
+
+# prepare indexfs-hdfs runtime if necessary
+if test x"$INDEXFS_BACKEND" = x"__HDFS__"
+then
+  LD_PATH=`$INDEXFS_HOME/sbin/hdfs.sh ldpath`
+  if test -n "$LD_LIBRARY_PATH"
+  then
+    LD_PATH=$LD_PATH:$LD_LIBRARY_PATH
+  fi
+  export LD_LIBRARY_PATH=$LD_PATH
+  export LIBHDFS_OPTS="-Djava.library.path=$LD_PATH"
+  export CLASSPATH=`$INDEXFS_HOME/sbin/hdfs.sh classpath`
+fi
+
+# use mpich if possible
+which mpiexec.mpich &>/dev/null
+if test $? -eq 0
+then
+  MPIEXEC=mpiexec.mpich
+else
+  MPIEXEC=mpiexec
+fi
+
+# advanced tree test settings
+NUM_CLIENTS=${1-"2"}
+FILE_ROOT="$INDEXFS_ROOT/_DATA_"
+DB_ROOT="$INDEXFS_ROOT/_META_"
+case $INDEXFS_RUN_TYPE in
+  batch-mknod)
+    extra_opts="--batch_creates=true"
+    ;;
+  no-write)
+    extra_opts="--enable_prepare=false --enable_main=false"
+    ;;
+  bulk-insert)
+    extra_opts="--enable_clean=false --bulk_insert=true"
+    ;;
+  read-old)
+    extra_opts="--enable_main=false --bulk_insert=true --local_stat=false"
+    extra_opts="$extra_opts --db_home_str=c --db_data_str=l --db_data_partitions=$NUM_CLIENTS"
+    ;;
+  *)
+    ;;
+esac
+
+set -x
+
+$MPIEXEC -np $NUM_CLIENTS $INDEXFS_BUILD/io_test/io_driver \
+  --prefix=$INDEXFS_RUN_PREFIX \
+  --task=cache \
+  --dirs=1 \
+  --files=8000 \
+  --share_dirs \
+  --ignore_errors=false \
+  --file_dir=$FILE_ROOT \
+  --db_root=$DB_ROOT \
+  --configfn=$INDEXFS_CONF_FILE \
+  --srvlstfn=$INDEXFS_SERVER_LIST \
+  --log_dir=$INDEXFS_OUTPUT \
+  --log_file=$INDEXFS_OUTPUT/perf_report \
+  $extra_opts
+
+exit 0
